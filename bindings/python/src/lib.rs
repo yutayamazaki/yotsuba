bdf239c6d7d5dc24075dfa20e402288ea7abe788
yutayamazaki <tppymd@gmail.com>
2020-08-18 01:57:01 +0900

    Add pad_sequence for python binding

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,6 +1,6 @@
 use pyo3::prelude::*;
 use pyo3::{wrap_pyfunction};
-use neologd::normalize;
+use neologd;
 
 #[pyfunction]
 fn neologd_normalize(text: &str) -> PyResult<String> {
@@ -8,15 +8,16 @@ fn neologd_normalize(text: &str) -> PyResult<String> {
     Ok(ret)
 }
 
-// #[pyfunction]
-// fn pad_sequence(text: String) -> PyResult<String> {
-//     let ret = neologd::normalize(&text);
-//     Ok(ret)
-// }
+#[pyfunction]
+fn pad_sequence(sequence: Vec<i32>, maxlen: i32, value: i32) -> PyResult<Vec<i32>> {
+    let ret = neologd::pad_sequence(&sequence, maxlen, value);
+    Ok(ret)
+}
 
 #[pymodule]
 fn yotsuba(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(neologd_normalize))?;
+    m.add_wrapped(wrap_pyfunction!(pad_sequence))?;
 
     Ok(())
 }
